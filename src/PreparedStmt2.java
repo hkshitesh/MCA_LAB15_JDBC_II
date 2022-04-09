da3b393356ef9d46861cef71ee7842b1e7d5f7f8
Hitesh <hkshitesh@gmail.com>
2022-04-09 10:29:38 +0530

    "second commit"

--- src/PreparedStmt2.java
+++ src/PreparedStmt2.java
@@ -3,32 +3,38 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
-public class PreparedSTMT {
+public class PreparedStmt2 {
 
 	public static void main(String[] args) throws SQLException {
-		
 		Scanner sc = new Scanner(System.in);
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?characterEncoding=utf8","root","hkshitesh");  			 
 				
-			int id;			
+			int id;
+			String name="";
 			System.out.println("Enter ID");
 			id=sc.nextInt();
-			String s = "select name from emp2 where id=?";
+			System.out.println("Enter Name");
+			name=sc.next();
+			String s="insert into emp2 value(?,?)";
 			PreparedStatement pstmt = con.prepareStatement(s);
-			pstmt.setInt(1, id);						
-			ResultSet rs=pstmt.executeQuery();  
+			pstmt.setInt(1, id);
+			pstmt.setString(2, name);
+			pstmt.executeUpdate();
+			ResultSet rs=pstmt.executeQuery("select * from emp2");  
 			while(rs.next())  
-			System.out.println(rs.getString(1));		 
+			{
+			System.out.println(rs.getInt(1)+" "+rs.getString(2));		
+			}
 			con.close();  			
 		} 
 		catch (ClassNotFoundException e) {			
 			e.printStackTrace();
 		} 
+
 	}
 
 }
